9c1d1a326aff6a819ed2528fe609033c15a8e08a
Florence Bain <florence_bain@outlook.com>
2021-12-20 12:34:36 +0000

    make data types explicit

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const todoitem_1 = require("./todoitem");
+const todoCollection_1 = require("./todoCollection");
+let todos = [
+    new todoitem_1.TodoItem(1, "Water plants"),
+    new todoitem_1.TodoItem(2, "Laundry"),
+    new todoitem_1.TodoItem(3, "Buy train tickets"),
+    new todoitem_1.TodoItem(4, "Call mum", true),
+];
+let collection = new todoCollection_1.TodoCollection("Florence", todos);
 console.clear();
-console.log("Florence's ToDo List");
+console.log(`${collection.userName}'s Todo List`);
+let newId = collection.addTodo("Go for run");
+let todoItem = collection.getTodoById(newId);
+todoItem.printDetails();
+//collection.addTodo(todoItem);
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 import { TodoItem } from "./todoitem";
 import { TodoCollection } from "./todoCollection";
 
-let todos = [
+let todos: TodoItem[] = [
   new TodoItem(1, "Water plants"),
   new TodoItem(2, "Laundry"),
   new TodoItem(3, "Buy train tickets"),
   new TodoItem(4, "Call mum", true),
 ];
 
-let collection = new TodoCollection("Florence", todos);
+let collection: TodoCollection = new TodoCollection("Florence", todos);
 
 console.clear();
 console.log(`${collection.userName}'s Todo List`);
 
-let newId = collection.addTodo("Go for run");
-let todoItem = collection.getTodoById(newId);
-console.log(JSON.stringify(todoItem));
+let newId: number = collection.addTodo("Go for run");
+let todoItem: TodoItem = collection.getTodoById(newId);
+todoItem.printDetails();
+
+//collection.addTodo(todoItem);
