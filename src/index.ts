b3c8079f75a24f2b436d8e6268f3065c2c5f1ee8
Florence Bain <florence_bain@outlook.com>
2021-12-20 12:15:18 +0000

    testing the data model"

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
+import { TodoItem } from "./todoitem";
+import { TodoCollection } from "./todoCollection";
+
+let todos = [
+  new TodoItem(1, "Water plants"),
+  new TodoItem(2, "Laundry"),
+  new TodoItem(3, "Buy train tickets"),
+  new TodoItem(4, "Call mum", true),
+];
+
+let collection = new TodoCollection("Florence", todos);
+
 console.clear();
-console.log("Florence's ToDo List");
+console.log(`${collection.userName}'s Todo List`);
+
+let newId = collection.addTodo("Go for run");
+let todoItem = collection.getTodoById(newId);
+console.log(JSON.stringify(todoItem));
