270430b781e8aa76ad83c0724854c9eb1d2c40e8
Florence Bain <florence_bain@outlook.com>
2021-12-20 14:29:08 +0000

    creating access to todo items

--- src/todoCollection.ts
+++ src/todoCollection.ts
@@ -2,10 +2,10 @@ import { TodoItem } from "./todoitem";
 
 export class TodoCollection {
   private nextId: number = 1;
-  private itemMap = new Map<number, TodoItem();
+  private itemMap = new Map<number, TodoItem>();
 
   constructor(public userName: string, public todoItems: TodoItem[] = []) {
-      todoItems.forEach(item => this.itemMap.set(item.id, item));
+    todoItems.forEach((item) => this.itemMap.set(item.id, item));
   }
 
   addTodo(task: string): number {
@@ -17,6 +17,12 @@ export class TodoCollection {
   }
   getTodoById(id: number): TodoItem {
     return this.itemMap.get(id);
+  }
+
+  getTodoItems(includeComplete: boolean): TodoItem[] {
+    return [...this.itemMap.values()].filter(
+      (item) => includeComplete || !item.complete
+    );
   }
 
   markComplete(id: number, complete: boolean) {
