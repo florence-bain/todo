e76ae98d484be5112ce53b2bd8f26714082e8602
Florence Bain <florence_bain@outlook.com>
2021-12-21 11:08:52 +0000

    add function to remove completed tasks

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,5 @@ let collection: TodoCollection = new TodoCollection("Florence", todos);
 console.clear();
 console.log(`${collection.userName}'s Todo List`);
 
-// let newId: number = collection.addTodo("Go for run");
-// let todoItem: TodoItem = collection.getTodoById(newId);
-// todoItem.printDetails();
-
+collection.removeComplete();
 collection.getTodoItems(true).forEach((item) => item.printDetails());
